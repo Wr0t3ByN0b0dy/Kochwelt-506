7215f51b9afdff8db59bfe4f5053a605c195118e
Azael <59912489+Wr0t3ByN0b0dy@users.noreply.github.com>
2025-08-06 15:48:54 +0200

    edit: spaghetti-carbonara.html

--- Kochwelt/zutaten_tabelle.js
+++ Kochwelt/zutaten_tabelle.js
@@ -1,18 +1,31 @@
 function hintergrundfarbe(tabelle) {
-let zeilen = tabelle.getElementsByTagName("tr");
+  let zeilen = tabelle.getElementsByTagName("tr");
 
-for (let index = 0; index < zeilen.length; index++) {
-  if (index % 2 !== 1) {
-    zeilen[index].classList.add("table-bg");
-  }   
+  for (let index = 0; index < zeilen.length; index++) {
+    if (index % 2 !== 1) {
+      zeilen[index].classList.add("table-bg");
+    }
+  }
 }
-}
 
-let ingredientsForPierogi = ['g Weizenmehl', 'Ei', 'Prisen Salz für den Nudelteig', 'g Kartoffeln, mehlig kochend', 'Salz', 'g Schichtkäse', 'Zwiebel', 'g Butter', 'Pfeffer', 'Zweige Petersilie', 'g Schmand', 'Mehl, für die Arbeitsfläche' ]
-let amountForPierogi = [250, 1, 0.5, 200, null, 150, 1, 50, null, 4, 50, null]
+let ingredientsForPierogi = [
+  "g Weizenmehl",
+  "Ei",
+  "Prisen Salz für den Nudelteig",
+  "g Kartoffeln, mehlig kochend",
+  "Salz",
+  "g Schichtkäse",
+  "Zwiebel",
+  "g Butter",
+  "Pfeffer",
+  "Zweige Petersilie",
+  "g Schmand",
+  "Mehl, für die Arbeitsfläche",
+];
+let amountForPierogi = [250, 1, 0.5, 200, null, 150, 1, 50, null, 4, 50, null];
 
-function IngredientPierogi(event){
-  event.preventDefault()
+function IngredientPierogi(event) {
+  event.preventDefault();
 
   let tabelle = document.getElementById("number-of-ingredient-for-pierogi");
   let input = document.getElementById("amountInput");
@@ -24,27 +37,39 @@ function IngredientPierogi(event){
     if (amountForPierogi[index] == null) {
       tabelle.innerHTML += `<tr><td>${ingredientsForPierogi[index]}</td></tr>`;
       continue;
-    } 
-    if (ingredientsForPierogi[index].startsWith ('g')) {
-      let newAmount = amountForPierogi[index] * multiplier / 4;
-      tabelle.innerHTML += `<tr><td>${newAmount + ingredientsForPierogi[index]}</td></tr>`;
+    }
+    if (ingredientsForPierogi[index].startsWith("g")) {
+      let newAmount = (amountForPierogi[index] * multiplier) / 4;
+      tabelle.innerHTML += `<tr><td>${
+        newAmount + ingredientsForPierogi[index]
+      }</td></tr>`;
     } else {
-      let newAmount = amountForPierogi[index] * multiplier / 4;
-      tabelle.innerHTML += `<tr><td>${newAmount + " " + ingredientsForPierogi[index]}</td></tr>`;
+      let newAmount = (amountForPierogi[index] * multiplier) / 4;
+      tabelle.innerHTML += `<tr><td>${
+        newAmount + " " + ingredientsForPierogi[index]
+      }</td></tr>`;
     }
   }
-  hintergrundfarbe(tabelle)
+  hintergrundfarbe(tabelle);
 }
 
-let ingredientsForSpaghetti = ['g Spaghetti oder Tortellini', 'g Schinken', 'Eigelb', 'g Butter', 'Prise Salz und Pfeffer', 'Prise Muskat', 'Parmesan']
-let amountForSpaghetti = [100, 50, 1, 12.5, 1, 0.25, 1]
+let ingredientsForSpaghetti = [
+  " g Spaghetti",
+  "Scheibe/n Speck",
+  "Ei",
+  "Eigelb",
+  " g Parmesan, gerieben",
+  "n.B. Salz und Pfeffer",
+];
+let amountForSpaghetti = [125, 1, 1, 0.5, 15, null];
 
-function IngredientSpaghetti(event){
-  event.preventDefault()
-  
-  let tabelle = document.getElementById("number-of-ingredient-for-spaghetti");
-  let input = document.getElementById("amountInput");
-  let multiplier = input.value;
+function IngredientSpaghetti(event) {
+  event.preventDefault();
+
+  let tabelle = document.getElementById("ingredients-table");
+  let input = document.getElementById("amountInput").value;
+
+  if (input === NaN || input > 15 || input <= 0) return;
 
   tabelle.innerHTML = "";
 
@@ -52,24 +77,39 @@ function IngredientSpaghetti(event){
     if (amountForSpaghetti[index] == null) {
       tabelle.innerHTML += `<tr><td>${ingredientsForSpaghetti[index]}</td></tr>`;
       continue;
-    } 
-    if (ingredientsForSpaghetti[index].startsWith ('g')) {
-      let newAmount = amountForSpaghetti[index] * multiplier;
-      tabelle.innerHTML += `<tr><td>${newAmount + ingredientsForSpaghetti[index]}</td></tr>`;
+    }
+    if (ingredientsForSpaghetti[index].startsWith("g")) {
+      let newAmount = amountForSpaghetti[index] * input;
+      tabelle.innerHTML += `<tr><td>${
+        newAmount + ingredientsForSpaghetti[index]
+      }</td></tr>`;
     } else {
-      let newAmount = amountForSpaghetti[index] * multiplier;
-      tabelle.innerHTML += `<tr><td>${newAmount + " " + ingredientsForSpaghetti[index]}</td></tr>`;
+      let newAmount = amountForSpaghetti[index] * input;
+      tabelle.innerHTML += `<tr><td>${
+        newAmount + " " + ingredientsForSpaghetti[index]
+      }</td></tr>`;
     }
   }
-  hintergrundfarbe(tabelle)
+  hintergrundfarbe(tabelle);
 }
 
-let ingredientsForSalad = ['Salatgurke(n)', 'Paprikaschot(n), rot und grün', 'g Tomate(n)', 'Zwiebel(n)', 'g Schafskäse(n)', 'Glas Oliven, schwarze, ca 100g', 'Salz und Pfeffer', 'Zitrone(n)', 'ml Olivenöl', 'Oregano']
-let amountForSalad = [1, 2, 500, 2, 200, 1, null, 1, 125, null]
+let ingredientsForSalad = [
+  "Salatgurke(n)",
+  "Paprikaschot(n), rot und grün",
+  "g Tomate(n)",
+  "Zwiebel(n)",
+  "g Schafskäse(n)",
+  "Glas Oliven, schwarze, ca 100g",
+  "Salz und Pfeffer",
+  "Zitrone(n)",
+  "ml Olivenöl",
+  "Oregano",
+];
+let amountForSalad = [1, 2, 500, 2, 200, 1, null, 1, 125, null];
 
-function IngredientSalad(event){
-  event.preventDefault()
-  
+function IngredientSalad(event) {
+  event.preventDefault();
+
   let tabelle = document.getElementById("number-of-ingredient-for-salad");
   let input = document.getElementById("amountInput");
   let multiplier = input.value;
@@ -80,24 +120,57 @@ function IngredientSalad(event){
     if (amountForSalad[index] == null) {
       tabelle.innerHTML += `<tr><td>${ingredientsForSalad[index]}</td></tr>`;
       continue;
-    } 
-    if (ingredientsForSalad[index].startsWith ('g') || ingredientsForSalad[index].startsWith ('ml')) {
-      let newAmount = amountForSalad[index] * multiplier / 4;
-      tabelle.innerHTML += `<tr><td>${newAmount + ingredientsForSalad[index]}</td></tr>`;
+    }
+    if (
+      ingredientsForSalad[index].startsWith("g") ||
+      ingredientsForSalad[index].startsWith("ml")
+    ) {
+      let newAmount = (amountForSalad[index] * multiplier) / 4;
+      tabelle.innerHTML += `<tr><td>${
+        newAmount + ingredientsForSalad[index]
+      }</td></tr>`;
     } else {
-      let newAmount = amountForSalad[index] * multiplier / 4;
-      tabelle.innerHTML += `<tr><td>${newAmount + " " + ingredientsForSalad[index]}</td></tr>`;
+      let newAmount = (amountForSalad[index] * multiplier) / 4;
+      tabelle.innerHTML += `<tr><td>${
+        newAmount + " " + ingredientsForSalad[index]
+      }</td></tr>`;
     }
   }
-  hintergrundfarbe(tabelle)
+  hintergrundfarbe(tabelle);
 }
 
-let ingredientsForGulasch = ['g Rindergulsch', 'g Zwiebeln', 'ml Rinderfond', 'g Karotten', 'Stück Paprikaschote(n)', 'Stück Knoblauchzehe', 'g Butterschmalz', 'Teelöffel Tomatenmark', 'Teelöffel Paprikapulver edelsüß', 'Petersilie frisch und glatt', 'Spritzer Zitrone', 'Majoran, Kümmel ganz, Loorbeerblätter, Salz und Pfeffer nach Bedarf']
-let amountForGulasch = [125, 75, 300, 38, 0.5, 0.25, 12.5, 1, 1, null, null, null]
+let ingredientsForGulasch = [
+  "g Rindergulsch",
+  "g Zwiebeln",
+  "ml Rinderfond",
+  "g Karotten",
+  "Stück Paprikaschote(n)",
+  "Stück Knoblauchzehe",
+  "g Butterschmalz",
+  "Teelöffel Tomatenmark",
+  "Teelöffel Paprikapulver edelsüß",
+  "Petersilie frisch und glatt",
+  "Spritzer Zitrone",
+  "Majoran, Kümmel ganz, Loorbeerblätter, Salz und Pfeffer nach Bedarf",
+];
+let amountForGulasch = [
+  125,
+  75,
+  300,
+  38,
+  0.5,
+  0.25,
+  12.5,
+  1,
+  1,
+  null,
+  null,
+  null,
+];
 
-function IngredientGulasch(event){
-  event.preventDefault()
-  
+function IngredientGulasch(event) {
+  event.preventDefault();
+
   let tabelle = document.getElementById("number-of-ingredient-for-gulasch");
   let input = document.getElementById("amountInput");
   let multiplier = input.value;
@@ -108,19 +181,25 @@ function IngredientGulasch(event){
     if (amountForGulasch[index] == null) {
       tabelle.innerHTML += `<tr><td>${ingredientsForGulasch[index]}</td></tr>`;
       continue;
-    } 
-    if (ingredientsForGulasch[index].startsWith ('g') || ingredientsForGulasch[index].startsWith ('ml')) {
+    }
+    if (
+      ingredientsForGulasch[index].startsWith("g") ||
+      ingredientsForGulasch[index].startsWith("ml")
+    ) {
       let newAmount = amountForGulasch[index] * multiplier;
-      tabelle.innerHTML += `<tr><td>${newAmount + ingredientsForGulasch[index]}</td></tr>`;
+      tabelle.innerHTML += `<tr><td>${
+        newAmount + ingredientsForGulasch[index]
+      }</td></tr>`;
     } else {
       let newAmount = amountForGulasch[index] * multiplier;
-      tabelle.innerHTML += `<tr><td>${newAmount + " " + ingredientsForGulasch[index]}</td></tr>`;
+      tabelle.innerHTML += `<tr><td>${
+        newAmount + " " + ingredientsForGulasch[index]
+      }</td></tr>`;
     }
   }
-  hintergrundfarbe(tabelle)
+  hintergrundfarbe(tabelle);
 }
 
 function toggleMenu() {
-      document.getElementById("menu-box")
-      .classList.toggle("mobile-menu-closed");
-    }
+  document.getElementById("menu-box").classList.toggle("mobile-menu-closed");
+}
